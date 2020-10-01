132a4e108b81bf80bdb62f58c0f372cc0455e659
wanni0928 <chlgudwnd123@gmail.com>
2020-10-01 14:02:16 +0900

    feat: add springContainer

--- src/main/java/com/spring/basic/spring_demo/MemberApp.java
+++ src/main/java/com/spring/basic/spring_demo/MemberApp.java
@@ -3,12 +3,18 @@ package com.spring.basic.spring_demo;
 import com.spring.basic.spring_demo.member.Grade;
 import com.spring.basic.spring_demo.member.Member;
 import com.spring.basic.spring_demo.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1l, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/main/java/com/spring/basic/spring_demo/OrderApp.java
+++ src/main/java/com/spring/basic/spring_demo/OrderApp.java
@@ -5,15 +5,21 @@ import com.spring.basic.spring_demo.member.Member;
 import com.spring.basic.spring_demo.member.MemberService;
 import com.spring.basic.spring_demo.order.Order;
 import com.spring.basic.spring_demo.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
 //        MemberService memberService = new MemberServiceImpl(null);
 //        OrderService orderService = new OrderServiceImpl(null, null);
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
